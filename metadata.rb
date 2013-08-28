d2a23274d7ff75dcea23c3ae54e94dac750354f4
Michael Hayes <tasslehawf@mac.com>
2013-08-28 15:46:11 -0500

    default recipe

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.5"
 %w(runit java iptables).each { |cb| depends cb }
+
+recipe "default"
