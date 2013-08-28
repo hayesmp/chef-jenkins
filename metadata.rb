62ce6a82ece08fd6596a4805eaf0140ae0bb373f
Michael Hayes <tasslehawf@mac.com>
2013-08-28 16:16:52 -0500

    install jenkins!!

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,14 @@
+name             "chef-box"
 maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.5"
-%w(runit java iptables).each { |cb| depends cb }
 
-recipe "default"
+recipe "chef-box"
+
+supports 'ubuntu'
+supports 'debian'
+
+%w(runit java iptables).each { |cb| depends cb }
