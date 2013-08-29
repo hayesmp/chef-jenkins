000da1a024504593fd726147a09631f5f7f3a2f2
Michael Hayes <tasslehawf@mac.com>
2013-08-29 11:39:26 -0500

    include_recipe 'jenkins'

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,3 @@
-name             "chef-box"
 maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
@@ -6,9 +5,6 @@ description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.5"
 
-recipe "chef-box"
-
-supports 'ubuntu'
-supports 'debian'
-
 %w(runit java iptables).each { |cb| depends cb }
+
+include_recipe 'jenkins'
